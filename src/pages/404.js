2439c8f11b27c3c32c3e107dfc88a859e3358823
liyo1242 <liyo1242@gmail.com>
2020-01-25 10:34:07 +0800

    404page & start goal fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,33 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styles from "../css/404.module.css"
+import { Link } from "gatsby"
 
 const Error = () => {
+  useEffect(() => {
+    let container = document.getElementById("404container")
+    window.onmousemove = function(e) {
+      let x = -e.clientX / 5,
+        y = -e.clientY / 5
+      container.style.backgroundPositionX = x + "px," + (15 + x) + "px"
+      container.style.backgroundPositionY = y + "px," + (15 + y) + "px"
+    }
+  })
+
   return (
     <Layout>
       <SEO title="404: Not Found" />
-      <h1>This is error page</h1>
+      <div id="404container" className={styles.container}>
+        <div className={styles.content}>
+          <h1>404</h1>
+          <h4>This is Error page</h4>
+          <p>Please come to the main entrance :D</p>
+          <Link to="/" className={styles.link}>
+            Back to Home
+          </Link>
+        </div>
+      </div>
     </Layout>
   )
 }
